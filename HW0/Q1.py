ffd05a4bd21be6c360fd76e5f11522464d0cdb83
Kogby <kogby0507@gmail.com>
2024-03-11 05:45:17 +0800

    [add] HW0

---
+++ HW0/Q1.py
@@ -0,0 +1,6 @@
+base = int(input())
+x1 = int(input())
+x2 = int(input())
+y1 = int(input())
+y2 = int(input())
+print(y1 + y2, x1 * y1 + x2 * y2 + base, sep=',')
---
+++ HW0/Q2.py
@@ -0,0 +1,8 @@
+base = int(input())
+x1 = int(input())
+x2 = int(input())
+y1 = int(input())
+y2 = int(input())
+total =  x1 * y1 + x2 * y2 + base
+
+print(y1 + y2, total, total % 10, sep=',')
